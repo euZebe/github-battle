9c5f0b84732d8dd0002bf2ab4df117b4db6b285d
euZebe <euzebe@axiome.me>
2018-11-19 22:56:15 +0100

    Ci config (#5)
    * add auto-authentication
    
    * init CI
    
    * init CI
    
    * ignore cypress folder for unit tests
    
    * trying to find the path to prevent npm test to execute cypress tests
    
    * init CI
    
    * try to launch cypress tests
    
    * remove travis env variables print
    
    * fix tests

--- cypress/integration/battle-results.spec.js
+++ cypress/integration/battle-results.spec.js
@@ -1,12 +1,12 @@
 describe("battle results page", () => {
   it("should require logging in", () => {
-    cy.visit("/battle/results?playerOneName=react&playerTwoName=vue");
+    cy.visit("/battle/results?playerOneName=angular&playerTwoName=vuejs");
     cy.get("[data-test=please_login_msg]");
   });
 
   it('should display results when logged in', () => {
     cy.login();
-    cy.visit('/battle/results?playerOneName=react&playerTwoName=vue');
+    cy.visit('/battle/results?playerOneName=angular&playerTwoName=vuejs');
     cy.get('[data-test=submitted_player]').should('have.length', 2);
     // Note: checking the way a player card is displayed is none of the e2e business
   });
