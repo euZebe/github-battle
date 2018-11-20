94d0cf91dcdcccdbe88ea52c61bf850332090000
euzebe <euzebe@axiome.me>
2018-11-20 06:38:26 +0100

    fix tests

--- cypress/integration/battle.spec.js
+++ cypress/integration/battle.spec.js
@@ -1,3 +1,6 @@
+const playerOne = "angular";
+const playerTwo = "vuejs";
+
 describe("battle page", () => {
   it("should require logging in", () => {
     cy.visit("/battle");
@@ -8,13 +11,13 @@ describe("battle page", () => {
     cy.login();
     cy.visit('/battle');
     cy.get('form');
-    cy.get('[data-test=username]').first().type('angular');
+    cy.get('[data-test=username]').first().type(playerOne);
     cy.get('button[type=submit]').first().click();
-    cy.get('[data-test=username]').first().type('vuejs');
+    cy.get('[data-test=username]').first().type(playerTwo);
     cy.get('button[type=submit]').first().click();
 
     cy.get('[data-test=submit_battle]').click(); // starting battle
-    cy.url().should('eq', `${Cypress.config().baseUrl}/battle/results?playerOneName=react&playerTwoName=vue`);
+    cy.url().should('eq', `${Cypress.config().baseUrl}/battle/results?playerOneName=${playerOne}&playerTwoName=${playerTwo}`);
     cy.get('[data-test=submitted_player]').should('have.length', 2);
     // Note: checking the way a player card is displayed is none of the e2e business
   });
