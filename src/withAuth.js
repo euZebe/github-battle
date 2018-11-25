bebe753b216057194336ec811b607dbdf16c80da
euzebe <euzebe@axiome.me>
2018-11-25 21:32:41 +0100

    update slides

--- src/withAuth.js
+++ src/withAuth.js
@@ -6,7 +6,7 @@ export default function withAuth(WrappedComponent, auth) {
       return auth.isAuthenticated() ? (
         <WrappedComponent {...this.props} />
       ) : (
-        <h3><a onClick={auth.login} data-test="please_login_msg">please login to access this page</a></h3>
+        <a className='warning-msg' onClick={auth.login} data-test="please_login_msg">please login to access this page</a>
       );
     }
   };
