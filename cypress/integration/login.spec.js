13db5cd579ea805e0b1cddababc5b99918211a26
Jean Martineau <jean.martineau.external@sigfox.com>
2018-11-30 05:32:56 +0100

    init gh-pages

--- cypress/integration/login.spec.js
+++
@@ -1,5 +0,0 @@
-describe('login feature', () => {
-  it('should be tested only when it is not delegated to a third-party server', () => {
-    cy.log("nothing to test on UI since it is delegated");
-  })
-});
